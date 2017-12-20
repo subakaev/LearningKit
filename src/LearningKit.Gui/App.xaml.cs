cf029bbd6bf2bbdc19897decdb14798d418162f7
Sergey Bakaev <bakaev@yandex.ru>
2017-12-20 21:45:35 +0300

    Configure Autofac

--- src/LearningKit.Gui/App.xaml.cs
+++ src/LearningKit.Gui/App.xaml.cs
@@ -5,13 +5,17 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using LearningKit.Gui.Startup;
 
 namespace LearningKit.Gui
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
-    {
+    public partial class App {
+        
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+        }
     }
 }
---
+++ src/LearningKit.Gui/Startup/AutofacContainer.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+
+namespace LearningKit.Gui.Startup
+{
+    static class AutofacContainer
+    {
+        public static readonly IContainer Container;
+
+        static AutofacContainer() {
+            var builder = new ContainerBuilder();
+
+            Container = builder.Build();
+        }
+
+        public static T Resolve<T>() {
+            return Container.Resolve<T>();
+        }
+    }
+}
